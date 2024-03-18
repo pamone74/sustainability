1eefd165050fc8f9cbfb1d3b0e01a2676752ed90
Pro_G <140634743+pamone74@users.noreply.github.com>
2024-03-18 09:15:21 +0400

    Delete mysite directory

--- mysite/Products/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-
-# Create your views here.
-class ModelDetailView(generic.DetailView):
-    model = Model
-    #template_name = ".html"
-    def __str__(self):
-        return self.model
